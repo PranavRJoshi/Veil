bc319c9a3bb6ff761806f3c0d6e687f02a68325f
PranavRJoshi <pranavrjoshi1@gmail.com>
2026-04-10 19:43:08 +0545

    add: EventKind type and Event structure
    EventKind identifies the kernel subsystem which emitted the event.
    
    Event is the "generic" structure that is used by the currently defined EventKind.
    
    Also introduce the String() method for EventKind, and ProcessName() for Event.

---
+++ internal/events/types.go
@@ -0,0 +1,64 @@
+package events
+
+import "fmt"
+
+/*
+	EventKind type identifies which kernel subsystem the event came from.
+*/
+type EventKind uint8
+
+/*
+	Supported kernel subsystems for EventKind.
+*/
+const (
+	KindSyscall EventKind = iota
+	KindNetwork
+	KindFileAccess
+	KindScheduler
+	KindMemory
+)
+
+/*
+	String() method for EventKind used to return the stringified version of the enumeration defined above.
+*/
+func (k EventKind) String() string {
+	switch k {
+		case KindSyscall:
+			return "syscall"
+		case KindNetwork:
+			return "network"
+		case KindFileAccess:
+			return "file access"
+		case KindScheduler:
+			return "scheduler"
+		case KindMemory:
+			return "memory"
+		default:
+			return fmt.Sprintf("unknown (%d)", k)
+	}
+}
+
+/*
+	The common "envelope" for everything coming out of the kernel.
+*/
+type Event struct {
+	Kind			EventKind
+	Pid				uint32
+	Tid				uint32
+	Comm			[16]byte		// kernel gives us 15 chars and 1 null terminator
+	Timestamp		uint64
+}
+
+/*
+	ProcessName() method of Event structure.
+	A null terminator check is done, and the slice is returned,
+	else all 16 bytes of Comm field is returned.
+*/
+func (e Event) ProcessName() string {
+	for i, b := range e.Comm {
+		if b == 0 {
+			return string(e.Comm[:i])
+		}
+	}
+	return string(e.Comm[:])
+}
