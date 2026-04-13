addaf978b0765fc3344798b029e32ead4f7836f9
PranavRJoshi <pranavrjoshi1@gmail.com>
2026-04-13 21:33:58 +0545

    update: kernscope/main.go to provide a simple example to log sys_enter tracepoint
    Signed-off-by: PranavRJoshi <pranavrjoshi1@gmail.com>

--- cmd/kernscope/main.go
+++ cmd/kernscope/main.go
@@ -3,24 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/PranavRJoshi/go-kernscope/internal/events"
+	"github.com/PranavRJoshi/go-kernscope/internal/loader"
+	ksyscall "github.com/PranavRJoshi/go-kernscope/modules/syscall"
 )
 
-func main () {
-	e := events.Event {
-		Kind:		events.KindSyscall,
-		Pid:		1234,
-		Tid:		1234,
-		Timestamp:	9999999,
+func main() {
+	tracer := ksyscall.New()
+
+	m := loader.NewManager()
+	m.Register(tracer)
+
+	if err := m.LoadAll(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-	copy(e.Comm[:], "bash");
+	defer m.CloseAll()
 
-	fmt.Printf("[%s] pid = %-6d comm = %s\n",
-			e.Kind,
-			e.Pid,
-			e.ProcessName(),
-		)
+	fmt.Println("kscope running, press ctrl+c to stop")
 
-	_ = os.Stdout
+	/*
+		A simple goroutine which traces the events that has occurred and
+		prints it to the standard output.
+	*/
+	go func() {
+		for e := range tracer.Events {
+			fmt.Printf("[%s] pid=%-6d comm=%s syscall=%s\n",
+				e.Kind,
+				e.PID,
+				e.ProcessName(),
+				ksyscall.SyscallName(e.SyscallNr),
+			)
+		}
+	}()
+
+	/*
+		Block the process till we are sent the SIGINT signal.
+	*/
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
 }
