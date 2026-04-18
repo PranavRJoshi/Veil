c41c033f1050ce1809609171fdb54539e3be58ce
PranavRJoshi <pranavrjoshi1@gmail.com>
2026-04-18 22:59:36 +0545

    feat: introduce command-line parsing
    Also uses the Run method to consume event from the channel and print to standard output stream.
    
    Instead of enabling all the modules, only enables the one selected by user.
    
    Signed-off-by: PranavRJoshi <pranavrjoshi1@gmail.com>

--- cmd/veil/main.go
+++ cmd/veil/main.go
@@ -6,62 +6,91 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/PranavRJoshi/Veil/internal/cli"
 	"github.com/PranavRJoshi/Veil/internal/loader"
 	ksyscall "github.com/PranavRJoshi/Veil/modules/syscall"
 	kfiles "github.com/PranavRJoshi/Veil/modules/files"
 )
 
 func main() {
-	tracer := ksyscall.New()
-	files := kfiles.New()
 
-	m := loader.NewManager()
-	m.Register(tracer)
-	m.Register(files)
-
-	if err := m.LoadAll(); err != nil {
+	cfg, err := cli.Parse(os.Args[1:])
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		cli.Usage()
 		os.Exit(1)
 	}
-	defer m.CloseAll()
+ 
+	if cfg.ShowHelp {
+		cli.Usage()
+		os.Exit(0)
+	}
+ 
+	if cfg.ListModules {
+		cli.PrintModules()
+		os.Exit(0)
+	}
 
-	fmt.Println("Veil running, press ctrl+c to stop")
+	m := loader.NewManager()	
 
 	/*
-		Goroutine whose task is to collect the syscall events
-		and print them out
+		done is closed when the user sends SIGINT/SIGTERM. Module
+		Run methods select on this channel to know when to exit.
 	*/
-	go func() {
-		for e := range tracer.Events {
-			fmt.Printf("[%s] pid=%-6d comm=%s syscall=%s\n",
-				e.Kind,
-				e.PID,
-				e.ProcessName(),
-				ksyscall.SyscallName(e.SyscallNr),
-			)
-		}
-	}()
+	done := make(chan struct{})
 
 	/*
-		Goroutine whose task is to collect the file access events
-		and print them out
+		Instantiate and register only the selected module. The
+		module's Run method is started as a goroutine after loading.
 	*/
-	go func() {
-		for e := range files.Events {
-			fmt.Printf("[%s] pid=%-6d comm=%-16s op=%-6s path=%s\n",
-				e.Kind,
-				e.PID,
-				e.ProcessName(),
-				e.Op,
-				e.Path,
-			)
+	switch cfg.Module {
+	case "syscall":
+		/* create the filter based on given command line arguments */
+		filter, err := ksyscall.ParseFilterConfig(cfg.ModuleFlags)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
 		}
-	}()
 
+		tracer := ksyscall.New(filter)
+		m.Register(tracer)
+ 
+		if err := m.LoadAll(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		defer m.CloseAll()
+ 
+		go tracer.Run(done)
+ 
+	case "files":
+		filter, err := kfiles.ParseFilterConfig(cfg.ModuleFlags)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+
+		files := kfiles.New(filter)
+		m.Register(files)
+ 
+		if err := m.LoadAll(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		defer m.CloseAll()
+ 
+		go files.Run(done)
+	}
+
+	fmt.Fprintf(os.Stderr, "Veil [%s] running, press ctrl+c to stop\n", cfg.Module)
+ 
 	/*
-		Block the process till we are sent the SIGINT signal.
+		Block the process till we are sent the SIGINT or SIGTERM signal.
+		Once received, close the done channel to signal goroutines to exit.
 	*/
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 	<-stop
+ 
+	close(done)
 }
