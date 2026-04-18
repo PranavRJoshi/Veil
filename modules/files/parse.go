e05bed824b2ce5687161d4372104a7a4e7d69512
PranavRJoshi <pranavrjoshi1@gmail.com>
2026-04-18 22:49:43 +0545

    feat: introduce UID and GID for files module
    Adjust the length of the fields and update the filename parsing field function.
    
    Signed-off-by: PranavRJoshi <pranavrjoshi1@gmail.com>

--- modules/files/parse.go
+++ modules/files/parse.go
@@ -8,15 +8,18 @@ import (
 )
 
 /*
-	The following structure must match the one defined in file_access.bpf.c
+	The following structure must match the one defined in file_access.bpf.c,
+	i.e., the file-based event sent by the bpf program.
 */
 type fileEvent struct {
-	PID       uint32
-	TID       uint32
-	Timestamp uint64
-	Comm      [16]byte
-	Path      [256]byte
-	Op        uint8
+	PID         uint32
+	TID         uint32
+	UID         uint32
+	GID         uint32
+	Timestamp   uint64
+	Comm        [16]byte
+	FileName    [256]byte
+	Op          uint8
 }
 
 /*
@@ -24,21 +27,23 @@ type fileEvent struct {
 */
 func opName(op uint8) string {
 	switch op {
-	case 0:
-		return "open"
-	case 1:
-		return "read"
-	case 2:
-		return "write"
-	default:
-		return fmt.Sprintf("op_%d", op)
+		case 0:
+			return "open"
+		case 1:
+			return "read"
+		case 2:
+			return "write"
+		default:
+			return fmt.Sprintf("op_%d", op)
 	}
 }
 
 /*
-	Parse the path.
+	Parse the filename. The size of filename field is large for most files so
+	we need to slice the byte array. If it occupies all the bytes, then we
+	simply return the entire byte array as a string.
 */
-func parsePath(raw [256]byte) string {
+func parseFileName(raw [256]byte) string {
 	for i, b := range raw {
 		if b == 0 {
 			return string(raw[:i])
@@ -51,28 +56,36 @@ func parsePath(raw [256]byte) string {
 	Parse the file event information that was received.
 */
 func parseEvent(raw []byte) (events.FileEvent, error) {
-	if len(raw) < 281 {
+	/*
+		The actual size may be higher due to padding between fields,
+		but we don't take that into account.
+	*/
+	if len(raw) < 297 {
 		return events.FileEvent{}, fmt.Errorf("short read: %d bytes", len(raw))
 	}
 
 	fe := fileEvent{
 		PID:       binary.LittleEndian.Uint32(raw[0:4]),
 		TID:       binary.LittleEndian.Uint32(raw[4:8]),
-		Timestamp: binary.LittleEndian.Uint64(raw[8:16]),
-		Op:        raw[280],
+		UID:       binary.LittleEndian.Uint32(raw[8:12]),
+		GID:       binary.LittleEndian.Uint32(raw[12:16]),
+		Timestamp: binary.LittleEndian.Uint64(raw[16:24]),
+		Op:        raw[296],
 	}
-	copy(fe.Comm[:], raw[16:32])
-	copy(fe.Path[:], raw[32:288])
+	copy(fe.Comm[:], raw[24:40])
+	copy(fe.FileName[:], raw[40:296])
 
 	return events.FileEvent{
 		Event: events.Event{
 			Kind:      events.KindFileAccess,
 			PID:       fe.PID,
 			TID:       fe.TID,
+			UID:       fe.UID,
+			GID:       fe.GID,
 			Timestamp: fe.Timestamp,
 			Comm:      fe.Comm,
 		},
-		Path: parsePath(fe.Path),
-		Op:   opName(fe.Op),
+		FileName:      parseFileName(fe.FileName),
+		Op:            opName(fe.Op),
 	}, nil
 }
